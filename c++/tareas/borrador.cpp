4c6926a739a315111f145fff1586c6ebe47ae3a9
hoyner3 <hoyner26@gmail.com>
2023-02-20 21:17:15 -0600

    tarea 3

--- c++/tareas/borrador.cpp
+++ c++/tareas/borrador.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
-#include <ctime>
-using namespace std;
-int main() {
-    time_t now = time(nullptr); // get current time
-    tm* timeinfo = localtime(&now); // convert to local time
+#include <list>
+#include <stdlib.h> // para la función rand()
+#include <time.h> // para la función time()
 
-  cout << "Hora actual es : " << timeinfo->tm_hour << ":" << timeinfo->tm_min << ":" << timeinfo->tm_sec << endl;
-  return 0;
+using namespace std;
+struct listas {
+    int numero;
+    listas *siguiente;
+
+    listas(int num) {
+        numero = num;
+        siguiente = NULL;
+    }
+};
+
+void insertarFinal(int num, listas *&lista){
+    listas *nuevo = new listas(num);
+    if (lista == NULL){
+        lista = nuevo;
+    }else{
+        listas *aux = lista;
+        while (aux->siguiente != NULL){
+            aux = aux->siguiente;
+        }
+        aux->siguiente = nuevo;
+    }
+}
+
+void llenarListaRandom(int cantidad, list<int> &lista) {
+    srand(time(NULL)); // inicializar la semilla para generar números aleatorios
+    
+    for (int i = 0; i < cantidad; i++) {
+        int num = rand(); // generar un número aleatorio
+        lista.push_back(num); // agregar el número a la lista
+    }
+}
+
+int main() {
+    list<int> lista;
+    llenarListaRandom(10, lista); // llenar la lista con 10 números aleatorios
+    for (auto num : lista) {
+        cout << num << " "; // imprimir los números de la lista
+    }
+    cout << endl;
+    
+    return 0;
 }
